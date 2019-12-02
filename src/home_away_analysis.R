f0bf1cbdc51112cb49d42d54762c41da7caf7696
SueShen7 <xs1065@nyu.edu>
2019-12-01 21:05:32 -0500

    keep writing

--- src/home_away_analysis.R
+++ src/home_away_analysis.R
@@ -4,8 +4,9 @@
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
+library(ROCR)
 
-### read and join data ###
+### READ AND JOIN DATA ###
 
 game_away <- read_csv('data/processed/game_away.csv')
 game_home <- read_csv('data/processed/game_home.csv')
@@ -23,7 +24,7 @@ game_away <- game_away %>%
 game_home <- game_home %>% 
   left_join(player_game_level)
 
-### exploratory research ###
+### EXPLORATORY RESEARCH ###
 
 # Is there substancial difference in scores between home and visitor teams?
 # Plot win or loss for home and visitor teams respectively with point size
@@ -105,8 +106,77 @@ p4.4 <- ggplot(plot.data, aes(x=as.factor(home), y=def_rtg.mean)) +
 p4 <- ggarrange(p4.1,p4.2,p4.3,p4.4, ncol=2, nrow=2)
 ggsave(plot=p4, file='figures/p4_defensive_performance.png', width = 10, height = 8)
 
-# exploratory research 4: any difference in offensive performance 
-# (personal fouls, steals, blocks, defensive rating)?
+# exploratory research 5: any difference in strategy?
+p5.1 <- ggplot(plot.data, aes(x=as.factor(home), y=fg3a_per_fga_pct.mean)) +
+  geom_boxplot() +
+  labs(x='home')
+p5.2 <- ggplot(plot.data, aes(x=as.factor(home), y=fta_per_fga_pct.mean)) +
+  geom_boxplot() +
+  labs(x='home')
+p5.3 <- ggplot(plot.data, aes(x=as.factor(home), y=tov.total)) +
+  geom_boxplot() +
+  labs(x='home')
+p5.4 <- ggplot(plot.data, aes(x=as.factor(home), y=usg_pct.mean)) +
+  geom_boxplot() +
+  labs(x='home')
+p5 <- ggarrange(p5.1,p5.2,p5.3,p5.4, ncol=2, nrow=2)
+ggsave(plot=p5, file='figures/p5_strategy.png', width = 10, height = 8)
+
+# exploratory research 6: any difference in energy?
+starter_mp <- player %>%
+  filter(starters==1) %>% 
+  group_by(date,team) %>% 
+  summarise(starter_min = mean(mp))
+game_home <- game_home %>% 
+  left_join(starter_mp)
+game_away <- game_away %>% 
+  left_join(starter_mp)
+plot.data = rbind(game_home, game_away)
+
+mean(game_home$starter_min)
+mean(game_away$starter_min)
+p6 <- ggplot(plot.data, aes(x=as.factor(home), y=starter_min)) +
+  geom_boxplot() +
+  labs(x='home')
+ggsave(plot=p6, file='figures/p6_starter_energy.png', width = 5, height = 5)
+
+
+### DEVELOP LOGISTIC REGRESSION MODEL ###
+
+# formula and two models
+formula_home = "win~attendance+
+                fg_pct.mean+fg3_pct.mean+
+                orb.total+drb.total+
+                pf.total+stl.total+blk.total+def_rtg.mean+
+                starter_min"
+formula_away = "win~distance+attendance+
+                fg_pct.mean+fg3_pct.mean+
+                orb.total+drb.total+
+                pf.total+stl.total+blk.total+def_rtg.mean+
+                starter_min"
+
+model_home_train <- game_home %>% slice(1:round(0.8*nrow(game_home)))
+model_home_test <- game_home %>% slice(round(0.8*nrow(game_home))+1:nrow(game_home))
+model_home <- glm(formula_home, data = model_home_train, family = 'binomial')
+summary(model_home)
+
+model_away_train <- game_away %>% slice(1:round(0.8*nrow(game_home)))
+model_away_test <- game_away %>% slice(round(0.8*nrow(game_home))+1:nrow(game_home))
+model_away <- glm(formula_away, data = model_away_train, family = 'binomial')
+summary(model_away)
+
+# performance of two models
+predicted_prob1 = predict(model_home, model_home_test, type = 'response')
+perf <- prediction(predicted_prob1, model_home_test$win)
+auc1 <- performance(perf, "auc")
+auc1 <- auc1@y.values[[1]]
+auc1
+
+predicted_prob2 = predict(model_away, model_away_test, type = 'response')
+perf <- prediction(predicted_prob2, model_away_test$win)
+auc2 <- performance(perf, "auc")
+auc2 <- auc2@y.values[[1]]
+auc2
 
 
 
